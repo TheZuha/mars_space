cdc84926dbcc593e93c7a773d14b895c248f1416
TheZuha <144546739+TheZuha@users.noreply.github.com>
2024-11-17 21:27:21 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%q+3!jh++e#@1@h55ril8=g(@!yroa&yg!dd*=szmjbdtfk@#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
